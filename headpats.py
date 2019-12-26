8775543c14f3c4691ba30ce7f50011fe130548f7
Swirya <55079804+heichou2210@users.noreply.github.com>
2019-12-27 00:05:13 +0100

    Create headpats.py

---
+++ headpats.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style, deinit, init
+
+init()
+
+print(Fore.BLUE + Style.BRIGHT + 'Lemme patpat youuuuu -w- do u wanna some headpats ? [Yes/No]')
+plop = input()
+
+if plop == "Yes":
+	print("Patpatpatpatpatpatpat >w<")
+elif plop == "No":
+	print("Oh... ;w;")
+else :
+	print("Uhm, okay :thinking:")
+
+deinit()
