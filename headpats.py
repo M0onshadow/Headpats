9b8939e61963c09c1ee2c56d57eeb716e2678253
M0onshadow <55079804+M0onshadow@users.noreply.github.com>
2020-05-28 15:33:30 +0200

    Update headpats.py

--- headpats.py
+++ headpats.py
@@ -1,16 +1,44 @@
 from colorama import Fore, Style, deinit, init
+import time
+import random
+import signal
 
 init()
 
-print(Fore.BLUE + Style.BRIGHT + 'Lemme patpat youuuuu -w- do u wanna some headpats ? [Yes/No]')
-plop = input()
+class TimerException(Exception):
+    pass
 
-if plop == "Yes":
-	print("Patpatpatpatpatpatpat >w<")
-elif plop == "No":
-	print("Oh... ;w;")
-else :
-	print("Uhm, okay :thinking:")
+def timeout(a,b):
+    raise TimerException("TIMEOUT")
 
-deinit()
-exit()
+
+def randchoice():
+    t = random.shuffle(["Automatic choice = headpats !!! *patpatpatpatpatpatpat* >w<", "Automatic choice = Huggyyyyyyys !!! *huggy* >w<", "Automatic choice = Nothing : go to hell."])
+    return t
+    print(t)
+
+signal.signal(signal.SIGALRM, timeout)
+
+signal.alarm(5)
+
+try:
+    inp = input(Fore.BLUE + Style.BRIGHT + 'Lemme patpat youuuuu -w- do u wanna some headpats ? [Y/N].\r\nCountdown before an automatic choice : 5sec.\r\n')
+except TimerException:
+    inp = randchoice()
+
+print()
+
+if inp == "Y":
+    print(Fore.RED + Style.BRIGHT + "Patpatpatpatpatpatpat >w<")
+elif inp == "N":
+    try:
+        inp  = input(Fore.GREEN + Style.BRIGHT + "Oh... Mh, some huggys ? *^* [Y/N/Maybe]\r\n")
+    except TimerException:
+        inp = "default"
+    print()
+    if inp == "Y":
+        print(Fore.RED + Style.BRIGHT + "Huggyyyyyyys !!!")
+    if inp == "N":
+        print(Fore.YELLOW + Style.BRIGHT + "\'kay...")
+    if inp == "Maybe":
+        print("Pfff, are you braindead ?")
